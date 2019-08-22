20977f21fcf7f4065c9a54a44245141cc3dca659
tiluong <tiluong@ucsc.edu>
2019-08-21 22:55:41 -0700

    added jwt check func

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-function helloWorld() {
-  return "Hello World!"
-}
+/*
+Helper functions
+*/
+
+// Server functions 
+const jwt = require('express-jwt');
+const getPublicKey = require('./lib/getPublicKey');
 
 module.exports = {
-  helloWorld: helloWorld
+  foo: function () {
+    console.log("foo() function")
+  },
+  jwtCheck: function () {
+    return jwt({
+      secret: getPublicKey(env('AUTH0_DOMAIN')),
+      audience: env('RESOURCE_SERVER'),
+      algorithms: ['RS256'],
+      issuer: `https://${env('AUTH0_DOMAIN')}/`
+    })
+  }
 }
+
+// Client functions 
+
+
+// function helloWorld() {
+//     return "Hello World!"
+// }
+
+// module.exports = {
+//   helloWorld: helloWorld,
+//   anotherStr: anotherStr
+// }
